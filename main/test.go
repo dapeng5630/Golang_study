32877941000cdf5efc5c6bf9327bfa3b0607b7ba
You Name <h-lvfp01@vanke.com>
2022-12-13 10:05:56 +0800

    Golane 学习练习

---
+++ main/test.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Golang!!")
+	var age int
+	age = 18
+	fmt.Println(age)
+	var num int
+	fmt.Printf("num的类型是 %T", num)
+}
---
+++ main/test12.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!!")
+	var age int
+	age = 18
+	fmt.Println(age)
+}
